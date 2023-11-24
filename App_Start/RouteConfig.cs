a792d95c2447c3442579dc5f635b09b5dea915fe
PhucLee14 <lhphucth14@gmail.com>
2023-11-24 21:20:56 +0700

    config route

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,18 +14,59 @@ namespace WebApplication5
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WebApplication5.Controllers" }
+            );
+
+            routes.MapRoute(
+                name: "AdminDefault",
+                url: "Admin/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "YourNamespace.Controllers" }
+            );
+            
+            routes.MapRoute(
                 name: "MyRoute",
                 url: "{controller}/{action}/{id}",
                 defaults: new { action = "Index", id = UrlParameter.Optional },
                 namespaces: new[] { "WebApplication5.Controllers" }
             );
 
-
             routes.MapRoute(
                 name: "Admin",
                 url: "Admin/{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional },
+                defaults: new {Controller = "Home", action = "Index", id = UrlParameter.Optional },
                 namespaces: new[] { "MyApplication.Areas.MyArea.Controllers" }
+            );
+
+            routes.MapRoute(
+                name: "NhanViens",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new { Controller = "NhanViens", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "MyApplication.Areas.MyArea.Controllers" }
+            );
+
+            routes.MapRoute(
+               name: "KhachHang",
+               url: "Admin/{controller}/{action}/{id}",
+               defaults: new { Controller = "KhachHang", action = "Index", id = UrlParameter.Optional },
+               namespaces: new[] { "MyApplication.Areas.MyArea.Controllers" }
+            );
+
+            routes.MapRoute(
+               name: "Phong",
+               url: "Admin/{controller}/{action}/{id}",
+               defaults: new { Controller = "Phong", action = "Index", id = UrlParameter.Optional },
+               namespaces: new[] { "MyApplication.Areas.MyArea.Controllers" }
+            );
+
+            routes.MapRoute(
+               name: "LoaiPhong",
+               url: "Admin/{controller}/{action}/{id}",
+               defaults: new { Controller = "LoaiPhong", action = "Index", id = UrlParameter.Optional },
+               namespaces: new[] { "MyApplication.Areas.MyArea.Controllers" }
             );
         }
     }
